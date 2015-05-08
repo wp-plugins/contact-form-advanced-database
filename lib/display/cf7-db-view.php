123ffd7924ec1969a88d8b7410d9ccf5014eedf3
akosicb <akosicb@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-08 15:21:07 +0000

    Trying Plugin Updates
    git-svn-id: https://plugins.svn.wordpress.org/contact-form-advanced-database/tags/1.0.1@1156246 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ lib/display/cf7-db-view.php
@@ -0,0 +1,102 @@
+<?php add_thickbox(); ?>
+<div class="wrap">
+	<h2>Contact Form Advanced Database
+		<select id="cf7-selector">
+			<?php
+				$cf7Selector = $_GET['id'];
+				$args = array(	'post_type'			=> 'wpcf7_contact_form',
+								'posts_per_page'	=> -1,
+								'orderby'			=> 'title',
+								'order'				=> 'ASC');
+				$cf7Query = new WP_Query( $args );
+				if ( $cf7Query->have_posts() ) {
+					$incSel = 0;
+					while ( $cf7Query->have_posts() ) {
+						$cf7Query->the_post();
+						echo '<option value="'.get_the_ID().'" '.(($cf7Selector==get_the_ID())?'selected':'').'>' . get_the_title() . '</option>';
+						if(empty($cf7Selector)){
+							if($incSel==0){
+								$cf7Selector = get_the_ID();
+							}
+							$incSel++;
+						}
+					}
+				}
+			
+				wp_reset_postdata(); 
+			?>
+		</select>
+		
+	</h2>
+	 <div id="poststuff">
+		<?php 
+			$colKeys = array();
+			$cf7AdbArr = get_post_meta($cf7Selector,'cf7-adb-data',true);
+			
+			if(!empty($cf7AdbArr)):	
+		?>
+		<button class="button" id="adb-delete-button">Delete</button>
+		<button class="button" id="adb-export">
+			<a target="_blank" href="admin.php?page=cf7-adb-export-xls&id=<?php echo $cf7Selector; ?>">Export XLS</a>
+		</button>
+		<table class="wp-list-table widefat fixed media paginated" id="property-lead-table" data-page-length='10'>
+			<thead>
+				<th class="manage-column column-cb check-column"></th>
+				<?php 
+				unset($cf7AdbArr['_wpcf7'],$cf7AdbArr['_wpcf7_version'],$cf7AdbArr['_wpcf7_locale'],$cf7AdbArr['_wpcf7_unit_tag'],$cf7AdbArr['_wpnonce'],$cf7AdbArr['_wpcf7_is_ajax_call']);
+					foreach(array_keys($cf7AdbArr) as $cf7AdbData) {
+					$colKeys[] = $cf7AdbData;
+				?>
+					<th class="manage-column column-title sorted">
+						<span><?php echo $cf7AdbData; ?></span>				
+					</th>
+				<?php } ?>
+				<th></th>
+				<th></th>
+			</thead>
+
+			<tfoot>
+				<th class="manage-column column-cb check-column"></th>
+				<?php foreach(array_keys($cf7AdbArr) as $cf7AdbData) {?>
+					<th class="manage-column column-title sorted">
+							<span><?php echo $cf7AdbData; ?></span>				
+					</th>
+				<?php } ?>
+				<th></th>
+				<th></th>
+			</tfoot>
+			<tbody>
+				<?php 
+				foreach(get_post_meta($cf7Selector,'cf7-adb-data') as $leadData){ 
+					$thDiv = rand(1,1000).'_'.time().'_'.rand(1000,5000);
+				?>
+					<tr>
+						<td><input class="adb-chk" type="checkbox" data-status="0" data-id="<?php echo $cf7Selector; ?>" data-key="cf7-adb-data" value="<?php echo base64_encode(maybe_serialize($leadData)); ?>"></td>
+						<?php foreach($colKeys as $colKeysData){ ?>
+							<td><?php echo (strlen($leadData[$colKeysData]) > 60 )?substr($leadData[$colKeysData],0,60).'...':$leadData[$colKeysData]; ?></td>
+						<?php } ?>
+						<td>
+						<div id="<?php echo $thDiv; ?>" style="display:none;">
+							 <div>
+								  <?php foreach($colKeys as $colKeysData){ ?>
+									<div class="adb-per-line">
+										<div class="field-name"><?php echo strtoupper($colKeysData) ?></div>
+										<div class="field-value"><?php echo $leadData[$colKeysData] ?></div>
+									</div>
+								 <?php } ?>
+							 </div>
+
+						</div>
+						<a href="#TB_inline?width=600&height=550&inlineId=<?php echo $thDiv; ?>" title="Contact Form fields and value" class="view-button thickbox">View</a></td>
+						<td>
+							<span class="del-button" data-id="<?php echo $cf7Selector; ?>" data-key="cf7-adb-data" data-val="<?php echo base64_encode(maybe_serialize($leadData)); ?>">Delete</span>
+						</td>
+					</tr>
+				<?php } ?>
+			</tbody>
+		</table> 	
+		<?php else: ?>
+		No Data Available
+		<?php endif; ?>	
+	</div>
+</div>
